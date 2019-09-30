892c94d8697999709b0b390cc65f4f678419065e
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2019-09-30 23:45:34 +0500

    updating design and adding name change effect

---
+++ scripts/languages.js
@@ -0,0 +1,33 @@
+let name = document.querySelector("#name");
+
+function changeToJava() {
+  name.innerHTML = "String arslanMushtaq;";
+}
+
+function changeToKotlin() {
+  name.innerHTML = "val arslanMushtaq;";
+}
+
+function changeToHTML() {
+  name.innerHTML = "&lt;arslan&gt;&lt;&sol;mushtaq&gt;;";
+}
+
+function changeToCSS() {
+  name.innerHTML = ".arslan-mushtaq { }";
+}
+
+function changeToJS() {
+  name.innerHTML = "let arslanMushtaq;";
+}
+
+function changeToPhp() {
+  name.innerHTML = "$arslan_mushtaq;";
+}
+
+function changeToReact() {
+  name.innerHTML = "&lt;ArslanMushtaq &sol;&gt;";
+}
+
+function changeToPython() {
+  name.innerHTML = "arslanMushtaq";
+}
