6c48c4a9dc2962596b627a03a3962566a1573ab8
Arslan Mushtaq <arslanark@gmail.com>
2022-11-16 02:58:35 +0500

    update query method (#22)
    * feat: enable responsiveness for every breakpoint (#15)
    
    Co-authored-by: Arslan Mushtaq <arslan@onlinereadingtutor.com>
    
    * anim: add animation to social icons
    
    * fix: add wordpress public api for fetching blogs
    
    * feat: update blogs to fetch articles from wordpress
    
    * fix: remove extra details
    
    * update query for projects
    
    Co-authored-by: Arslan Mushtaq <arslan@onlinereadingtutor.com>

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,9 +1,23 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../firebase";
 
-export const fetchCollection = async (collectionName) => {
+export const fetchCollectionFromDB = async (collectionName) => {
   try {
     const querySnapshot = await getDocs(collection(db, collectionName));
+    const data = [];
+    for (const doc of querySnapshot.docs) {
+      data.push({ id: doc.id, ...doc.data() });
+    }
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const fetchProjectsFromDB = async () => {
+  try {
+    const q = query(collection(db, "projects"), where("released", "==", true));
+    const querySnapshot = await getDocs(q);
     const data = [];
     for (const doc of querySnapshot.docs) {
       data.push({ id: doc.id, ...doc.data() });
