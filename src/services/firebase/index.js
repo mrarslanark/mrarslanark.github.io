cd3ea6c6f6ddefc653339dcf214a5c0f1aefca48
Arslan Mushtaq <arslanark@gmail.com>
2022-11-16 23:32:59 +0500

    fix: fix firebase collection labels

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,18 +1,9 @@
 import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import { db } from "../../firebase";
 
-const {
-  REACT_APP_FIREBASE_PROJECTS,
-  REACT_APP_FIREBASE_SKILLS,
-  REACT_APP_FIREBASE_ACCOMPLISHMENTS,
-} = process.env;
-
 export const fetchProjectsFromDB = async () => {
   try {
-    const q = query(
-      collection(db, REACT_APP_FIREBASE_PROJECTS),
-      where("released", "==", true)
-    );
+    const q = query(collection(db, "projects"), where("released", "==", true));
     const querySnapshot = await getDocs(q);
     const data = [];
     for (const doc of querySnapshot.docs) {
@@ -26,10 +17,7 @@ export const fetchProjectsFromDB = async () => {
 
 export const fetchSkillsFromDB = async () => {
   try {
-    const q = query(
-      collection(db, REACT_APP_FIREBASE_SKILLS),
-      orderBy("experience", "desc")
-    );
+    const q = query(collection(db, "skills"), orderBy("experience", "desc"));
     const querySnapshot = await getDocs(q);
     const data = [];
     for (const doc of querySnapshot.docs) {
@@ -43,10 +31,7 @@ export const fetchSkillsFromDB = async () => {
 
 export const fetchAccomplishmentsFromDB = async () => {
   try {
-    const q = query(
-      collection(db, REACT_APP_FIREBASE_ACCOMPLISHMENTS),
-      orderBy("year", "desc")
-    );
+    const q = query(collection(db, "accomplishments"), orderBy("year", "desc"));
     const querySnapshot = await getDocs(q);
     const data = [];
     for (const doc of querySnapshot.docs) {
