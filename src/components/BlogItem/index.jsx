31803238423445db66cb38b2b790aa2b14cbf879
Arslan Mushtaq <arslanark@gmail.com>
2022-11-17 03:53:13 +0500

    fix: pass blog detail as params

--- src/components/BlogItem/index.jsx
+++ src/components/BlogItem/index.jsx
@@ -7,7 +7,7 @@ const BlogItem = ({ item }) => {
   const navigate = useNavigate();
 
   const onNavigate = () => {
-    navigate(`/blog/${item.id}`);
+    navigate(`/blog/${item.slug}`, { state: item });
   };
 
   return (
