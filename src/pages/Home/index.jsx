ea2f8ddfac81998493384baa81609639a5022e9f
Arslan Mushtaq <arslanark@gmail.com>
2023-02-02 18:16:12 +0500

    update head tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -63,11 +63,11 @@ const HeadTags = () => {
       <meta property="og:title" content="Arslan Mushtaq" />
       <meta
         property="og:image"
-        content="https://firebasestorage.googleapis.com/v0/b/portfolio-mrarslanark-github.appspot.com/o/og_image.jpeg?alt=media&token=a4f4bb8b-32c5-47fa-afa5-eb400471b1f2"
+        content="https://arslanmushtaq.com/images/site_preview.jpeg"
       />
       <meta property="og:image:alt" content="Welcome to my Website" />
       <meta property="og:description" content="Software Developer" />
-      <meta property="og:url" content="http://arslanmushtaq.com/" />
+      <meta property="og:url" content="https://arslanmushtaq.com/" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="627" />
       <meta property="og:type" content="website" />
