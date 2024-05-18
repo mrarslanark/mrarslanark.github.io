39785dfd428c2cef2884f6ca8e1e2b66aa26262d
Arslan Mushtaq <arslanark@gmail.com>
2024-05-18 12:01:44 +0400

    feat: add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
