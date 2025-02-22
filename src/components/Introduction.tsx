d80c125d8d58b24cb5d7d14d2f9519b459dd5f96
Arslan Mushtaq <arslanark@gmail.com>
2025-02-22 21:51:27 +0400

    chore: update dependencies and improve TypeScript configuration

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -8,28 +8,14 @@ import TextLink from "./TextLink";
 const Introduction = () => {
   return (
     <Section>
-      <div className="flex gap-x-2 items-center">
-        <div className="flex flex-1 flex-col gap-3">
-          <div className="flex flex-col gap-1">
-            <p className="text-md font-semibold md:text-2xl">ARSLAN MUSHTAQ</p>
-            <p className="text-xs md:text-base">
-              Full Stack Developer, Content Creator, Gamer, and Traveler
-            </p>
-          </div>
-          <SocialLinks />
+      <div className="flex flex-1 flex-col gap-3">
+        <div className="flex flex-col gap-1">
+          <p className="text-md font-semibold md:text-2xl">ARSLAN MUSHTAQ</p>
+          <p className="text-xs md:text-base">
+            Full Stack Developer, Content Creator, Gamer, and Traveler
+          </p>
         </div>
-        <a
-          href={Docs.Resume}
-          className="flex flex-col items-center gap-y-2 text-gray-400 hover:text-black cursor-pointer transition-colors hidden sm:flex"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <IoDocumentTextOutline size={45} />
-          <p className="font-bold">Resume</p>
-        </a>
-      </div>
-      <div className="sm:hidden mt-4">
-        <TextLink text="Resume" link={Docs.Resume} />
+        <SocialLinks />
       </div>
     </Section>
   );
